d48759c4cf7530f4d9ce02c5a64e700263e04722
Anupama-n <anupaneupane1994@gmail.com>
2023-11-08 22:36:08 +0545

    Initial commit

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+import './header.css'
+
+export default function () {
+  return (
+    <div className="header">
+        <div className="headerTitles">
+            <span className="headerTitleSm">Blog</span>
+            <span className="headerTitleLg">TechVerse</span>
+        </div>
+        <img className="headerImg"
+        //src="https://importanceoftechnology.net/wp-content/uploads/2020/10/Possible-Future-Technology-of-the-World.jpg"
+        //src="https://png.pngtree.com/background/20230823/original/pngtree-cute-robotic-team-working-on-computer-notebooks-in-an-automated-office-picture-image_4789383.jpg"
+       src="https://www.securitymagazine.com/ext/resources/Issues/2022/September/SEC-0922-News3-Feat-Slide1-1170x658.jpg?1663331758"
+       //src="https://images.unsplash.com/photo-1684369175809-f9642140a1bd?auto=format&fit=crop&q=80&w=1542&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+       alt=""/>
+
+    </div>
+  )
+}
